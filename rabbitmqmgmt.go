469fb1046e7312bae367d0124722c8fb8a313d69
Eduardo Ferro <eferro@alea-soluciones.com>
2014-07-02 18:39:07 +0200

    Flags for exchange and queue creation

--- rabbitmqmgmt.go
+++ rabbitmqmgmt.go
@@ -6,12 +6,11 @@ import (
 	"os"
 )
 
-
 func main() {
 	app := cli.NewApp()
 	app.Name = "rabbitmqmgmt"
-  app.Usage = "rabbitmq queue/exchage/bindings management"
-	
+	app.Usage = "rabbitmq queue/exchage/bindings management"
+
 	app.Commands = []cli.Command{
 		{
 			Name:      "queue",
@@ -21,8 +20,12 @@ func main() {
 				{
 					Name:  "add",
 					Usage: "add a new queue",
+					Flags: []cli.Flag{
+						cli.BoolFlag{"durable", "queue survive broker restart"},
+						cli.BoolFlag{"auto-delete", "queue is deleted when last consumer unsubscribes"},
+					},
 					Action: func(c *cli.Context) {
-						println("new queue: ", c.Args().First())
+						println("new queue: ", c.Args().First(), "durable", c.Bool("durable"), "auto-delete", c.Bool("auto-delete"))
 					},
 				},
 				{
@@ -34,32 +37,32 @@ func main() {
 				},
 			},
 		},
-    {
-      Name:      "exchange",
-      ShortName: "e",
-      Usage:     "options for queues",
-      Subcommands: []cli.Command{
-        {
-          Name:  "add",
-          Usage: "add a new exchange",
-          Flags: []cli.Flag {
-            cli.StringFlag{"type", "direct", "exchange type (direct|fanout|topic|Header)"},
-            cli.BoolFlag{"durable", "exchanges survive broker restart"},
-            cli.BoolFlag{"auto-delete", "exchange is deleted when all queues have finished using it"},
-          },
-          Action: func(c *cli.Context) {
-            println("new exchange: ", c.Args().First(), c.String("type"), "durable", c.Bool("durable"), "auto-delete", c.Bool("auto-delete"))
-          },
-        },
-        {
-          Name:  "remove",
-          Usage: "remove an existing exchange",
-          Action: func(c *cli.Context) {
-            println("removed exchange: ", c.Args().First())
-          },
-        },
-      },
-    },
+		{
+			Name:      "exchange",
+			ShortName: "e",
+			Usage:     "options for exchanges",
+			Subcommands: []cli.Command{
+				{
+					Name:  "add",
+					Usage: "add a new exchange",
+					Flags: []cli.Flag{
+						cli.StringFlag{"type", "direct", "exchange type (direct|fanout|topic|Header)"},
+						cli.BoolFlag{"durable", "exchanges survive broker restart"},
+						cli.BoolFlag{"auto-delete", "exchange is deleted when all queues have finished using it"},
+					},
+					Action: func(c *cli.Context) {
+						println("new exchange: ", c.Args().First(), c.String("type"), "durable", c.Bool("durable"), "auto-delete", c.Bool("auto-delete"))
+					},
+				},
+				{
+					Name:  "remove",
+					Usage: "remove an existing exchange",
+					Action: func(c *cli.Context) {
+						println("removed exchange: ", c.Args().First())
+					},
+				},
+			},
+		},
 	}
 	app.Run(os.Args)
 }
