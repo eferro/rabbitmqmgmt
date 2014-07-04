747b4c943f255bd4ae6905897017b4a736b194d0
Eduardo Ferro <eferro@alea-soluciones.com>
2014-07-04 08:45:29 +0200

    Exchange commands

--- rabbitmqmgmt.go
+++ rabbitmqmgmt.go
@@ -48,10 +48,47 @@ func queue_remove(amqp_uri string, queue_name string) {
 
 	_, err = ch.QueueDelete(
 		queue_name,
-		false,	// ifUnused
-		false,  // ifEmpty
-		false)   // noWait
-	failOnError(err, "Failed to declare a queue")
+		false, // ifUnused
+		false, // ifEmpty
+		false) // noWait
+	failOnError(err, "Failed to remove a queue")
+}
+
+func exchange_create(amqp_uri string, queue_name string, exchange_type string, durable bool, auto_delete bool) {
+	conn, err := amqp.Dial(amqp_uri)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	err = ch.ExchangeDeclare(
+		queue_name,
+		exchange_type,
+		durable,
+		auto_delete,
+		false, // internal
+		false, // noWait
+		nil,   // args
+	)
+	failOnError(err, "Failed to declare a exchange")
+}
+
+func exchange_remove(amqp_uri string, exchange_name string) {
+	conn, err := amqp.Dial(amqp_uri)
+	failOnError(err, "Failed to connect to RabbitMQ")
+	defer conn.Close()
+
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+	defer ch.Close()
+
+	err = ch.ExchangeDelete(
+		exchange_name,
+		false, // ifUnused
+		false) // noWait
+	failOnError(err, "Failed to remove exchange")
 }
 
 func main() {
@@ -101,14 +138,14 @@ func main() {
 						cli.BoolFlag{"auto-delete", "exchange is deleted when all queues have finished using it"},
 					},
 					Action: func(c *cli.Context) {
-						println("new exchange: ", c.Args().First(), c.String("type"), "durable", c.Bool("durable"), "auto-delete", c.Bool("auto-delete"))
+						exchange_create(amqp_uri, c.Args().First(), c.String("type"), c.Bool("durable"), c.Bool("auto-delete"))
 					},
 				},
 				{
 					Name:  "remove",
 					Usage: "remove an existing exchange",
 					Action: func(c *cli.Context) {
-						println("removed exchange: ", c.Args().First())
+						exchange_remove(amqp_uri, c.Args().First())
 					},
 				},
 			},
